66cb37c5abd64e4ec72698fa8da1e845729247a0
hcart15 <harry15carter@gmail.com>
2025-02-26 20:51:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_caching import Cache  # Import caching
 import joblib
 
 # Load CSV only once (instead of on every request)
-consolidated_data = pd.read_csv("data/consolidated_data_final_with_composite_boosts.csv")
+consolidated_data = pd.read_csv("consolidated_data_final_with_composite_boosts.csv")
 
 # Initialize Flask App
 app = Flask(__name__, template_folder="templates", static_folder="static")
